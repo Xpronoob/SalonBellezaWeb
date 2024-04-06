48b48f5f5d1cea8148cebbc2d81f76295ce54f6c
Srojas80278 <Srojas80278@gmail.com>
2024-04-06 15:29:30 -0600

    Se corrigen mensajes de validacion y de éxitos

--- app/Http/Requests/ProfileUpdateRequest.php
+++ app/Http/Requests/ProfileUpdateRequest.php
@@ -4,21 +4,48 @@ namespace App\Http\Requests;
 
 use App\Models\User;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Str; // Importar la clase Str
 use Illuminate\Validation\Rule;
 
 class ProfileUpdateRequest extends FormRequest
 {
     /**
-     * Get the validation rules that apply to the request.
+     * Prepare the data for validation.
      *
-     * @return array<string, \Illuminate\Contracts\Validation\Rule|array|string>
+     * @return void
      */
+    
+    protected function prepareForValidation() //Metodo para que no detecte las mayusculas del email.
+    {
+        $this->merge([
+            'email' => Str::lower($this->email), // Convertir el correo electrónico a minúsculas
+        ]);
+    }
+
     public function rules(): array
     {
         return [
             'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'lowercase', 'email', 'max:255', Rule::unique(User::class)->ignore($this->user()->id)],
+            'email' => ['required', 'lowercase', 'string', 'email', 'max:255', Rule::unique(User::class)->ignore($this->user()->id)],
             'phone' => ['required', 'numeric']
         ];
     }
+
+    public function messages(): array
+    {
+        return [
+            'name.required' => 'El nombre completo es requerido.',
+            'name.string' => 'El nombre debe ser una cadena de texto.',
+            'name.max' => 'El nombre no puede tener más de :max caracteres.',
+            'email.required' => 'El correo es requerido.',
+            'email.string' => 'El correo debe ser una cadena de texto.',
+            'email.email' => 'El formato del correo electrónico no es válido.',
+            'email.lowercase' => 'El correo debe estar en minúsculas.',
+            'email.max' => 'El correo no puede tener más de :max caracteres.',
+            'email.unique' => 'El correo ya ha sido tomado.',
+            'phone.required' => 'El teléfono es requerido.',
+            'phone.numeric' => 'El teléfono debe ser numérico.'
+        ];
+    }
+
 }
--- resources/views/profile/partials/update-profile-information-form.blade.php
+++ resources/views/profile/partials/update-profile-information-form.blade.php
@@ -26,7 +26,7 @@
 
         <div>
             <x-input-label for="email" :value="__('Correo Electrónico')" />
-            <x-text-input id="email" name="email" type="email" class="mt-1 block w-full" :value="old('email', $user->email)"
+            <x-text-input id="email" name="email" type="text" class="mt-1 block w-full" :value="old('email', $user->email)"
                 autocomplete="username" />
             <x-input-error class="mt-2" :messages="$errors->get('email')" />
 
@@ -64,10 +64,33 @@
         <div class="flex items-center gap-4">
             <x-primary-button>{{ __('Actualizar') }}</x-primary-button>
 
-            @if (session('status') === 'profile-updated')
-                <p x-data="{ show: true }" x-show="show" x-transition x-init="setTimeout(() => show = false, 2000)"
-                    class="text-sm text-gray-600">{{ __('Perfil Actualizado.') }}</p>
-            @endif
+@if (session('status') === 'profile-updated')
+    <div x-data="{ show: true }" x-show="show" x-transition:enter="transition ease-out duration-300"
+        x-transition:enter-start="opacity-0 transform translate-y-2"
+        x-transition:enter-end="opacity-100 transform translate-y-0"
+        x-transition:leave="transition ease-in duration-200"
+        x-transition:leave-start="opacity-100 transform translate-y-0"
+        x-transition:leave-end="opacity-0 transform translate-y-2"
+        x-init="setTimeout(() => show = false, 10000)"
+        class="rounded-md bg-green-50 p-4 mt-4">
+        <div class="flex">
+            <div class="flex-shrink-0">
+                <!-- Heroicon name: check-circle -->
+                <svg class="h-5 w-5 text-green-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"
+                    fill="currentColor" aria-hidden="true">
+                    <path fill-rule="evenodd"
+                        d="M10 18a8 8 0 100-16 8 8 0 000 16zm-1.293-9.293a1 1 0 011.414-1.414l2 2a1 1 0 01-1.414 1.414L9 11.414l-1.293-1.293a1 1 0 010-1.414z"
+                        clip-rule="evenodd" />
+                </svg>
+            </div>
+            <div class="ml-3">
+                <p class="text-sm font-medium text-green-800">
+                    {{ __('Perfil Actualizado.') }}
+                </p>
+            </div>
+        </div>
+    </div>
+@endif
         </div>
     </form>
 </section>
