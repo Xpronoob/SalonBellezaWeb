bdc0cda45327b6bb2d06aa4d9031d609e63415ab
Srojas80278 <Srojas80278@gmail.com>
2024-04-23 14:19:51 -0600

    Final Fixes Sebas

--- resources/views/vistaUsuario.blade.php
+++ resources/views/vistaUsuario.blade.php
@@ -40,7 +40,7 @@
     <!-- ======= Header ======= -->
     <header id="header" class="fixed-top">
         <div class="container d-flex align-items-center">
-            <h1 class="logo me-auto"><a href="index.html">Utopía Beauty Salón</a></h1>
+            <h1 class="logo me-auto"><a href="/">Utopía Beauty Salón</a></h1>
             <nav id="navbar" class="navbar">
                 <ul>
                     <li><a class="nav-link scrollto active" href="#hero">Principal</a></li>
